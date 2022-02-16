0f0f4fbcb35fdb4ff0cd1f92a72484c951ea1071
Andrew Wei <andrewwei913@gmail.com>
2022-02-16 15:44:18 -0800

    Added configuration files

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  # route to test your configuration
+  get '/hello', to: 'application#hello_world'
 end
