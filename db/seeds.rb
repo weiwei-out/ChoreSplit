acb31d3445a5f295c759c02761cd113f66551a42
mikegibson <grandmoffgibson@gmail.com>
2022-02-17 04:43:52 -0600

    Servers Good / Before Adding MUI

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-# puts "Deleting old data"
-
-
 User.create(name:"Andrew", nickname:"dumbfck", picture:"cow", password:"123")
 User.create(name:"Michael", nickname:"Germany", picture:"bird", password:"456")
 User.create(name:"Tyler", nickname:"Ty", picture:"ninja", password:"789")
